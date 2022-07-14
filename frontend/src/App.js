33d034ceb79ecc90d91a2107cf2fd883b5aad314
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-07-14 04:10:40 -0700

    bootswatch react-bootstrap cdn.js-fontawesome Header--navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import { Container } from "react-bootstrap";
+import "./bootstrap.min.css";
 const App = () => {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to VShop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 };
