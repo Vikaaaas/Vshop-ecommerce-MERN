50d8f17684637f369df5e72669a14975b81835af
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-07-15 06:14:07 -0700

    backend and fetch

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,18 @@
-import React from "react";
-import products from "../products";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../Components/Product";
+
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get("/api/products");
+      setProducts(data);
+    };
+    fetchProducts();
+  }, []);
   return (
     <>
       <h1>Latest Products</h1>
