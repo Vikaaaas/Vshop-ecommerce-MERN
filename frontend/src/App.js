898d44b163c6d6f490dab31f91435dba87c59de0
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-07-15 02:56:37 -0700

    front-end completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 import { Container } from "react-bootstrap";
+import ProductScreen from "./screens/ProductScreen";
 import "./bootstrap.min.css";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to VShop</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
