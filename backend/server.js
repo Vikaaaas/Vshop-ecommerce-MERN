953c444c3a4284445abeeeeef1dbb54cc62c31ca
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-07-20 11:25:51 -0700

    Used Postman and improved error handling

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 //import products from "./data/products.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 dotenv.config();
 
 connectDB();
@@ -14,6 +15,9 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
