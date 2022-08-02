bcfb866fe595aa355200f90b7f7c0623e2516a49
Vikas Singh Bhadoriya <vs6889498@gmail.com>
2022-08-01 20:57:37 -0700

    orderController and orderRoutes

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,9 @@ import connectDB from "./config/db.js";
 import colors from "colors";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 dotenv.config();
 
 connectDB();
@@ -18,6 +20,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
